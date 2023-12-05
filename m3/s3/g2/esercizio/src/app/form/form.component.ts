1cba43c2ba8b4ff31a0c5d0b0349acdc2e142732
AlessandroForestiero <forestieroalessandro94@gmail.com>
2023-12-05 16:53:16 +0100

    esercizio

---
+++ m3/s3/g2/esercizio/src/app/form/form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent implements OnInit{
+
+form!:FormGroup;
+
+
+
+constructor(private fb:FormBuilder){}
+
+ngOnInit(){
+  this.form = this.fb.group({
+  nome:this.fb.control(null,[Validators.required]),
+  cognome:this.fb.control(null,[Validators.required]),
+  password:this.fb.control(null,[Validators.required]),
+  confermaPassword:this.fb.control(null,[Validators.required]),
+  genere:this.fb.control(null,[(Validators.required)]),
+  img:this.fb.control(null,[(Validators.required)]),
+  bio:this.fb.control(null,[(Validators.required)]),
+  username:this.fb.control(null,[(Validators.required)])
+ }
+  )
+ }
+// ngOnInit(){
+//   this.form = new FormGroup({
+//   nome:new FormControl(null,[Validators.required]),
+//   cognome:new FormControl(null,[Validators.required]),
+//   password:new FormControl(null,[Validators.required]),
+//   confermaPassword:new FormControl(null,[Validators.required]),
+//   genere:new FormControl(null,[(Validators.required)]),
+//   img:new FormControl(null,[(Validators.required)]),
+//   bio:new FormControl(null,[(Validators.required)]),
+//   username:new FormControl(null,[(Validators.required)])
+//  }
+//   )
+//  }
+
+send(){
+  console.log(this.form.value);
+
+}
+}
